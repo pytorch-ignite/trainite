30614f59eac4674549a5b190e2e249d4f28bd498
Taha Zahid <tz09220@st.habib.edu.pk>
2026-06-12 18:57:21 +0500

    fix test

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -100,7 +100,7 @@ def test_generated_string_reversal_project_is_runnable() -> None:
 
         config["trainer"]["epochs"] = 1
         config["trainer"]["log_every_steps"] = 1
-        config["model"]["layers"] = 1
+        config["model"]["num_layers"] = 1
         config["model"]["hidden_size"] = 16
         config["model"]["feedforward_dim"] = 32
         config["model"]["num_heads"] = 2
