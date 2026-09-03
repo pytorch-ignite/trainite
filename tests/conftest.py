db20974cb52d7856b91912070e0a2c69b0c11456
Sara <sarazahran543@gmail.com.com>
2026-09-03 21:54:49 +0300

    Use pytest addopts for integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import torch.utils.data
 import pytest
 
-
 # Store the original init method
 _original_dataloader_init = torch.utils.data.DataLoader.__init__
 
@@ -17,20 +16,3 @@ def force_dataloader_num_workers_zero():
     torch.utils.data.DataLoader.__init__ = patched_dataloader_init
     yield
     torch.utils.data.DataLoader.__init__ = _original_dataloader_init
-
-
-def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
-    if config.option.markexpr:
-        return
-
-    integration_tests = []
-    regular_tests = []
-
-    for item in items:
-        if "integration" in item.keywords:
-            integration_tests.append(item)
-        else:
-            regular_tests.append(item)
-
-    items[:] = regular_tests
-    config.hook.pytest_deselected(items=integration_tests)
