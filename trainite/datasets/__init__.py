6d9847fced1505f9acd63685a640996db4aedba9
Sara <sarazahran543@gmail.com.com>
2026-09-12 21:57:26 +0300

    revert: remove init.py dataset imports and integration test change

--- trainite/datasets/__init__.py
+++ trainite/datasets/__init__.py
@@ -6,12 +6,10 @@ from trainite.datasets.counting import (
 )
 from trainite.datasets.hugging_face import HuggingFaceTransform
 from trainite.datasets.wikitext import WikiTextTransform
-from trainite.datasets.python_edu import PythonEduDataset
 
 __all__ = [
     "StringReverseDataset",
     "CountingDataset",
     "HuggingFaceTransform",
     "WikiTextTransform",
-    "PythonEduDataset",
 ]
